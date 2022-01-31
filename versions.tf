013f8b515a191208e4157bfdb125d118096971c0
Rodrigo Custodio <rodrigo.oliveira@meliuz.com.br>
2022-01-31 11:14:50 -0300

    chore: Adicione provider null-label

--- versions.tf
+++ versions.tf
@@ -1,3 +1,18 @@
+#terraform {
+#  required_version = ">= 0.13.0"
+#}
+
+
 terraform {
   required_version = ">= 0.13.0"
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.1.0"
+    }
+  }
+}
+
+provider "null" {
+  # Configuration options
 }
